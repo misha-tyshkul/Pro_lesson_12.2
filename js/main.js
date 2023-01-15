a36b35645f9942e6b5e0c7fbad31a890bc77390e
Misha Tyshkul <tyshkulm@gmail.com>
2023-01-15 18:56:29 +0200

    added cells

--- js/main.js
+++ js/main.js
@@ -7,34 +7,54 @@ function displayTable(event) {
   const tableData = document.createElement("table");
 
   const tr = document.createElement("tr");
-  tr.innerText = `Name: ${myForm.name.value}`;
+  const td = document.createElement("td");
+  td.innerText = `Name: ${myForm.name.value}`;
+  tr.append(td);
 
   const tr2 = document.createElement("tr");
-  tr2.innerText = `Last name: ${myForm.last_name.value}`;
+  const td2 = document.createElement("td");
+  td2.innerText = `Last name: ${myForm.last_name.value}`;
+  tr2.append(td2);
 
   const tr3 = document.createElement("tr");
-  tr3.innerText = `Birthday: ${myForm.birthday.value}`;
+  const td3 = document.createElement("td");
+  td3.innerText = `Birthday: ${myForm.birthday.value}`;
+  tr3.append(td3);
 
   const tr4 = document.createElement("tr");
-  tr4.innerText = `Sex: ${myForm.sex.value}`;
+  const td4 = document.createElement("td");
+  td4.innerText = `Sex: ${myForm.sex.value}`;
+  tr4.append(td4);
 
   const tr5 = document.createElement("tr");
-  tr5.innerText = `City: ${myForm.city.value}`;
+  const td5 = document.createElement("td");
+  td5.innerText = `City: ${myForm.city.value}`;
+  tr5.append(td5);
 
   const tr6 = document.createElement("tr");
-  tr6.innerText = `Adress: ${myForm.adress.value}`;
+  const td6 = document.createElement("td");
+  td6.innerText = `Adress: ${myForm.adress.value}`;
+  tr6.append(td6);
 
   const tr7 = document.createElement("tr");
-  tr7.innerText = `Language eng: ${myForm.eng.checked}`;
+  const td7 = document.createElement("td");
+  td7.innerText = `Language eng: ${myForm.eng.checked}`;
+  tr7.append(td7);
 
   const tr8 = document.createElement("tr");
-  tr8.innerText = `Language uk: ${myForm.uk.checked}`;
+  const td8 = document.createElement("td");
+  td8.innerText = `Language uk: ${myForm.uk.checked}`;
+  tr8.append(td8);
 
   const tr9 = document.createElement("tr");
-  tr9.innerText = `Language fr: ${myForm.fr.checked}`;
+  const td9 = document.createElement("td");
+  td9.innerText = `Language fr: ${myForm.fr.checked}`;
+  tr9.append(td9);
 
   const tr10 = document.createElement("tr");
-  tr10.innerText = `Language es: ${myForm.es.checked}`;
+  const td10 = document.createElement("td");
+  td10.innerText = `Language es: ${myForm.es.checked}`;
+  tr10.append(td10);
 
   tableData.append(tr);
   tableData.append(tr2);
